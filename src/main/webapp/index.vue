633d6a099ba67f081321b205bfb48c0a2a955fad
ejller <ejller@bk.ru>
2019-11-23 23:38:02 +0300

    shit front

---
+++ src/main/webapp/index.vue
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>start vue</title>
+</head>
+<body>
+<script src="/js/vue.js"></script>
+<div id="app">
+    <h1>Пошел нахуй {{name}}</h1>
+    <input type="text" v-on:input="changeName">
+</div>
+
+<script>
+    new Vue({
+        el:'#app',
+        data: {
+            name: 'Vue'
+        },
+        methods: {
+            changeName: function (event) {
+                this.name = event.target.value
+            }
+        }
+    })
+</script>
+</body>
+</html>
