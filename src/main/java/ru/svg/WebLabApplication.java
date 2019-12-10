06beedfb71cd995f77de3b95eb032baec65e869a
grigoriy <grisha.56@icloud.com>
2019-12-10 15:22:55 +0300

    Lil fix

--- src/main/java/ru/svg/WebLabApplication.java
+++ src/main/java/ru/svg/WebLabApplication.java
@@ -2,13 +2,10 @@ package ru.svg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
 
 @SpringBootApplication(scanBasePackages = {"ru.svg"})
 public class WebLabApplication {
     public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(WebLabApplication.class, args);
+        SpringApplication.run(WebLabApplication.class, args);
     }
 }
