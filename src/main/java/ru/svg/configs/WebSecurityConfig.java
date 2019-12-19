bf7d6ed758acb8d04939019f8f600c12ef142394
grigoriy <grisha.56@icloud.com>
2019-12-20 01:37:21 +0300

    Enable cors

--- src/main/java/ru/svg/configs/WebSecurityConfig.java
+++ src/main/java/ru/svg/configs/WebSecurityConfig.java
@@ -33,7 +33,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
